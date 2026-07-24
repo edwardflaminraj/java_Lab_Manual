dd5cad745c625e404c1fa8aa4e052187cda0391f
edwardflaminraj <edwardflaminraj2006@gmail.com>
2026-07-24 23:49:38 +0530

    Lab Manual

---
+++ DesignparkingSystem .java
@@ -0,0 +1,38 @@
+public class DesignparkingSystem {
+
+    private int[] parking;
+
+    public ParkingSystem(int big, int medium, int small) {
+        parking = new int[4];
+        parking[1] = big;
+        parking[2] = medium;
+        parking[3] = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (parking[carType] > 0) {
+            parking[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
+public class DesignparkingSystem {
+
+    private int[] parking;
+
+    public ParkingSystem(int big, int medium, int small) {
+        parking = new int[4];
+        parking[1] = big;
+        parking[2] = medium;
+        parking[3] = small;
+    }
+
+    public boolean addCar(int carType) {
+        if (parking[carType] > 0) {
+            parking[carType]--;
+            return true;
+        }
+        return false;
+    }
+}
